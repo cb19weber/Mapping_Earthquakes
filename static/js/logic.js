d94b1377fff0bc0619b5eb7b27d0066d5c57fafb
cb19weber <webercullen@gmail.com>
2022-09-12 09:25:15 -0600

    through module 13.5.5

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,5 @@
 // We create the first option tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -14,22 +14,29 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create the map object with a center and zoom level.
 let map = L.map('mapid', {
-    center: [40.7, -94.5],
-    zoom: 4,
-    layers: [streets]
+    center: [44.0, -80.0],
+    zoom: 2,
+    layers: [dark]
 });
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: streets,
+    Light: light,
     Dark: dark
   };
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the airport GeoJSON URL
+// Accessing the airport and Toronto routes GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/cb19weber/Mapping_Earthquakes/main/static/js/majorAirports.json";
+let torontoData = "https://raw.githubusercontent.com/cb19weber/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/static/js/torontoRoutes.json";
+
+// Create a universal style for the map lines
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+}
 
 // Grab our GeoJSON data
 d3.json(airportData).then(function(data) {
@@ -43,7 +50,17 @@ d3.json(airportData).then(function(data) {
     }).addTo(map);
 });
 
-
+// Grab our GeoJSON data
+d3.json(torontoData).then(function(data) {
+    console.log(data);
+    // Create a GeoJSON layer with the retreived data
+    L.geoJSON(data, {
+        style: myStyle,
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("<h3>Airline: " + layer.feature.properties.airline + "</h3><hr><h3>Destination: " + layer.feature.properties.dst + "</h3>")
+        }
+    }).addTo(map);
+});
 
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+light.addTo(map);
